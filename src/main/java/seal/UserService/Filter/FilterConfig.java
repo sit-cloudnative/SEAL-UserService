5a23721c720c84bad30e83af4fb741deaf15478c
tozzis <patcharapol.top@mail.kmutt.ac.th>
2018-11-16 12:34:28 +0700

    [ADD] Filter By Token

---
+++ src/main/java/seal/UserService/Filter/FilterConfig.java
@@ -0,0 +1,24 @@
+package seal.UserService.Filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class FilterConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers(HttpMethod.POST, "/user/login").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(
+                    new JWTGenericFilterBean(),
+                    UsernamePasswordAuthenticationFilter.class
+            );
+    }
+}
