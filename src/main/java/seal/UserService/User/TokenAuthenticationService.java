c26aa4fb005a3ba47767a668f51959fc47eefdcb
tozzis <patcharapol.top@mail.kmutt.ac.th>
2018-11-16 12:33:39 +0700

    [EDIT] edit getAuthentication in TokenAuthenticationService

--- src/main/java/seal/UserService/User/TokenAuthenticationService.java
+++ src/main/java/seal/UserService/User/TokenAuthenticationService.java
@@ -5,12 +5,16 @@ import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+
+import static java.util.Collections.emptyList;
 
 @Service
 public class TokenAuthenticationService {
@@ -31,20 +35,21 @@ public class TokenAuthenticationService {
         return token;
     }
 
-    public String getAuthenticationToken(HttpServletRequest request){
-        String token = request.getHeader("token");
+    public static Authentication getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token != null) {
+            // parse the token.
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token.replace("Bearer", ""))
+                    .getBody()
+                    .getSubject();
 
-        if(token == null) {
-            return null;
+            return user != null ?
+                    new UsernamePasswordAuthenticationToken(user, null, emptyList()) :
+                    null;
         }
-
-        String studentId = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-        
-        return studentId;
+        return null;
     }
 
 }
